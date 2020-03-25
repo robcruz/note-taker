8111c4bd4baa073515b840cc928883582e1b1878
Rob <alekseicruz@gmail.om>
2020-03-24 19:34:48 -0700

    Initial checkin

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
