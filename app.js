32189e9dc12fc7b609f9d261547f06850f7ad1e2
Rob <alekseicruz@gmail.om>
2020-04-06 11:47:33 -0700

    Implemented that annoying data-qa. I don't even know why it works but it just does lol god damn it.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 80;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
